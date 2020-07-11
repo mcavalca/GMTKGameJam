56b640e73e7855dfc62cd2379fe0fa6c6daa8d73
Marcelo Cavalca Filho <m.cavalca@hotmail.com>
2020-07-11 15:03:34 -0300

    agora VAI

---
+++ GMTKGameJam/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	private float playerSpeed = 5f;
+    private float moveDirection = 0f;
+    private Vector2 direction;
+    public GameObject playerObject;
+    private Rigidbody2D playerBody;
+
+    private void Awake() {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+    }
+
+    private void FixedUpdate() {
+        moveDirection = Input.GetAxisRaw("Horizontal");  
+        if (moveDirection != 0) {
+            playerObject.transform.Translate(moveDirection * playerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
