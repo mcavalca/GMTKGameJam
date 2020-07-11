3613cf8f4cb5a3b19181941cb3afd86ba2be24b5
Marcelo Cavalca Filho <m.cavalca@hotmail.com>
2020-07-11 15:32:46 -0300

    correct player script
    still need to fix interactions

--- GMTKGameJam/Assets/Scripts/PlayerControl.cs
+++ GMTKGameJam/Assets/Scripts/PlayerControl.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-	private float playerSpeed = 5f;
-    private float moveDirection = 0f;
-    private Vector2 direction;
-    public GameObject playerObject;
     private Rigidbody2D playerBody;
+    public GameObject playerObject;
+    public GameObject interactiveObject;
+
+	private float playerSpeed = 3f;
+    private float moveDirection = -1f;
+
+    private bool playerActionEnabled;
+    private int totalNumberOfActions = 0;
+    // private int currentActionNumber = 0;
+    private List<bool> actionToDo = new List<bool>();
+    private List<bool> actionsDone = new List<bool>();
+    private List<float> interactiveObjectPositionX = new List<float>();
 
     private void Awake() {
         playerBody = GetComponent<Rigidbody2D>();
+        playerActionEnabled = false;
+        foreach(Transform currentChild in interactiveObject.transform){
+            totalNumberOfActions++;
+            actionToDo.Add(false);
+            actionsDone.Add(false);
+            interactiveObjectPositionX.Add(currentChild.transform.position.x);
+        }
     }
 
     void Start() {
@@ -22,9 +37,25 @@ public class PlayerControl : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        moveDirection = Input.GetAxisRaw("Horizontal");  
-        if (moveDirection != 0) {
-            playerObject.transform.Translate(moveDirection * playerSpeed * Time.deltaTime, 0, 0);
+        for (int actionNumber = 0; actionNumber < totalNumberOfActions; actionNumber++)
+            StartCoroutine(ActionFunction(actionNumber));
+    }
+
+    private void MovePlayer() {
+        playerBody.gameObject.transform.Translate(playerSpeed * moveDirection * Time.deltaTime, 0, 0);
+    }
+
+    private IEnumerator ActionFunction(int currentAction) {
+        MovePlayer();
+        float playerPos = Math.abs(playerBody.gameObject.transform.position.x);
+        if(playerPos >= (interactiveObjectPositionX[currentAction] * moveDirection)) {
+            playerActionEnabled = true;
+            actionToDo[currentAction] = true;
+            moveDirection = 0f;
+            yield return new WaitForSecondsRealtime(2);
+            currentAction++;
+            moveDirection = interactiveObjectPositionX[currentAction];
         }
     }
+
 }
